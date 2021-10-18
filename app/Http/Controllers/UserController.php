1e224cb0ccbf695573f573bb598765379d7b7d7a
JuanR <juan.randazzo@correounivalle.edu.co>
2021-10-18 15:18:31 -0500

    Autenticacion funcionando

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -16,5 +17,29 @@ class UserController extends Controller
 
     public function store(UserRequest $request)
     {
+        $toConcat = strtolower($request->name);
+        $searchDuplicateUser = User::where('name', $request->name)->first();
+        if ($searchDuplicateUser) {
+            return response('', 400);
+        }
+        $newUser = User::make([
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+            'email' => "{$toConcat}@" . "gmail.com"
+        ]);
+        $newUser->save();
+        return response()->json($newUser);
+    }
+
+    public function auth(UserRequest $request)
+    {
+        $user = User::where('name', $request->name)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                return response('OK', 202);
+            }
+        } else {
+            return response('NOT OK', 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('v1/users', App\Http\Controllers\UserController::class);
+Route::post('v1/auth', [\App\Http\Controllers\UserController::class, 'auth']);
 Route::apiResource('v1/tasks', App\Http\Controllers\TaskController::class);
