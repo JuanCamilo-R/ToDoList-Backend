e6a5e4038a01efa4e8d5e9eb248c978455fc4af3
JuanR <juan.randazzo@correounivalle.edu.co>
2021-10-18 21:28:24 -0500

    Agregando tokens a la autenticacion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -20,15 +21,16 @@ class UserController extends Controller
         $toConcat = strtolower($request->name);
         $searchDuplicateUser = User::where('name', $request->name)->first();
         if ($searchDuplicateUser) {
-            return response('', 400);
+            return response('hola', 401);
         }
+        $hashedPassword = Hash::make($request->password);
         $newUser = User::make([
             'name' => $request->name,
-            'password' => Hash::make($request->password),
-            'email' => "{$toConcat}@" . "gmail.com"
+            'password' => $hashedPassword,
+            'email' => "{$toConcat}@" . "gmail.com",
         ]);
         $newUser->save();
-        return response()->json($newUser);
+        return response()->json($hashedPassword);
     }
 
     public function auth(UserRequest $request)
@@ -36,7 +38,9 @@ class UserController extends Controller
         $user = User::where('name', $request->name)->first();
         if ($user) {
             if (Hash::check($request->password, $user->password)) {
-                return response('OK', 202);
+                return response($user->password, 202);
+            } else {
+                return response('NOT OK', 400);
             }
         } else {
             return response('NOT OK', 400);
