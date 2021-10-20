9d85ee8d9731eb0a6263e37e5a39bc896de554c4
JuanR <juan.randazzo@correounivalle.edu.co>
2021-10-20 13:53:45 -0500

    Agregando clave API

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureTokenIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->header('API_TOKEN') !== env('API_TOKEN')) {
+            return response('Not ok', 401);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('v1/users', App\Http\Controllers\UserController::class);
-Route::post('v1/auth', [\App\Http\Controllers\UserController::class, 'auth']);
-Route::apiResource('v1/tasks', App\Http\Controllers\TaskController::class);
-Route::post('v1/auth/tasks', [\App\Http\Controllers\TaskController::class, 'auth']);
+Route::apiResource('v1/users', App\Http\Controllers\UserController::class)->middleware('auth.token');
+Route::post('v1/auth', [\App\Http\Controllers\UserController::class, 'auth'])->middleware('auth.token');
+Route::apiResource('v1/tasks', App\Http\Controllers\TaskController::class)->middleware('auth.token');
+Route::post('v1/auth/tasks', [\App\Http\Controllers\TaskController::class, 'auth'])->middleware('auth.token');
